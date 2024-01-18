86ce972ad425f975564fc57a9763a83aa606b9d4
aryan <at9120140@gmail.com>
2024-01-18 22:37:13 +0530

    commited

--- script.js
+++ script.js
@@ -9,29 +9,30 @@ let apiinfo = [];
 
 // https://goweather.herokuapp.com/weather/Dehradun
 const apicall = async (cityname) => {
-  let api = `https://goweather.herokuapp.com/weather/${cityname}`
-  try{
+  let api = `https://api.weatherapi.com/v1/current.json?key=%20d44783a50343446a9b8155230241801&q=${cityname}&aqi=no`
+  try {
     const response = await fetch(api);
     const json = await response.json();
-    apiinfo.push(json); 
+    apiinfo.push(json);
     console.log(apiinfo);
-    if (!apiinfo[0].temperature || apiinfo[0].temperature === '') {
+    if (apiinfo[0].error.code=="1006"||apiinfo[0].error.code=="1003") {
       citynameelement.innerHTML = 'City not found';
-      wind.innerHTML=''
-      temp.innerHTML=''
-      description.innerHTML=''
-      
+      wind.innerHTML = ''
+      temp.innerHTML = ''
+      description.innerHTML = ''
+
       // Add similar handling for other elements if needed
     }
-    else{
+    else {
       citynameelement.innerHTML = `${cityname}`;
-      wind.innerHTML=`${apiinfo[0].wind}`
-      description.innerHTML=`${apiinfo[0].description}`
+      temp.innerHTML = `${apiinfo[0].current.temp_c}°C`;
+      wind.innerHTML = `${apiinfo[0].current.wind_kph}`
+      description.innerHTML =`${apiinfo[0].current.condition.text}`
     }
-    
 
-  }catch(error){
-      console.log("error fetching data",error);
+
+  } catch (error) {
+    console.log("error fetching data", error);
   }
 }
 
